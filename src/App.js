faf8f715a07e3f4fd75b5b679c6b9aea565fd2d4
cheesefield <sharkbay@email.cz>
2021-12-07 16:35:11 +0100

    fix4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 // components
 import Nav from "./components/Nav";
@@ -16,13 +16,11 @@ function App() {
     <div className="App">
       <Router>
         <Nav />
-        <Routes>
-          <Route path="/" element={<Domu />} />
-          <Route path="/denni_menu" element={<DenniMenu />} />
-          <Route path="/stala_nabidka" element={<StalaNabidka />} />
-          <Route path="/o_nas" element={<ONas />} />
-          <Route path="/kontakt" element={<Kontakt />} />
-        </Routes>
+        <Route path="/" element={<Domu />} />
+        <Route path="/denni_menu" element={<DenniMenu />} />
+        <Route path="/stala_nabidka" element={<StalaNabidka />} />
+        <Route path="/o_nas" element={<ONas />} />
+        <Route path="/kontakt" element={<Kontakt />} />
         <Footer />
       </Router>
     </div>
