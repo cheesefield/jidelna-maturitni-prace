a449b0f27f6bfb0c4eadcf7d2772a39450c4abfd
cheesefield <sharkbay@email.cz>
2021-12-07 16:53:26 +0100

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 // components
 import Nav from "./components/Nav";
@@ -16,11 +16,13 @@ function App() {
     <div className="App">
       <Router>
         <Nav />
-        <Route path="/" element={<Domu />} />
-        <Route path="/denni_menu" element={<DenniMenu />} />
-        <Route path="/stala_nabidka" element={<StalaNabidka />} />
-        <Route path="/o_nas" element={<ONas />} />
-        <Route path="/kontakt" element={<Kontakt />} />
+        <Routes>
+          <Route path="/" element={<Domu />} />
+          <Route path="/denni_menu" element={<DenniMenu />} />
+          <Route path="/stala_nabidka" element={<StalaNabidka />} />
+          <Route path="/o_nas" element={<ONas />} />
+          <Route path="/kontakt" element={<Kontakt />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
